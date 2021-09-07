6bef244502c1cee0a8825c067b25554b87d78a13
Egor <edkuz11@yandex.ru>
2021-09-07 21:17:26 +0300

    Receiving message metric

--- app/fetch_client.py
+++ app/fetch_client.py
@@ -2,6 +2,7 @@ from telethon import TelegramClient, events
 
 from app import bot
 from app.logger import logger
+from app.metrics import Metrics
 import config
 
 client = TelegramClient("innopost", config.API_ID, config.API_HASH)
@@ -11,6 +12,8 @@ client = TelegramClient("innopost", config.API_ID, config.API_HASH)
 async def handler(event):
     if event.chat_id != config.CHAT_ID:
         return
+
+    Metrics.receiving_message.inc()
 
     message_text = event.message.raw_text
     message_link = f"https://t.me/c/{event.message.chat.id}/{event.message.id}"
