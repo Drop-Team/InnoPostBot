35cb66132c45415819d5629959d1683f9e9dff04
Egor <edkuz11@yandex.ru>
2021-10-17 20:55:31 +0300

    Prometheus port to config

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ from app import bot
 from app import fetch_client
 from prometheus_client import start_http_server
 
+import config
+
 
 def main():
-    start_http_server(8000)
+    start_http_server(config.PROMETHEUS_PORT)
     fetch_client.start()
     bot.start()
 
